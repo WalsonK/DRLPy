5703e40412c40a87ab30e4dfd703b3f02b118841
WalsonK <71435596+WalsonK@users.noreply.github.com>
2024-11-19 13:36:33 +0100

    Actor critic : Test function

--- models/reinforce_actor_critic.py
+++ models/reinforce_actor_critic.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import Sequential
 from environement.farkle import Farkle
 import numpy as np
 import random
-from tqdm import tqdm
+from tqdm import tqdm, trange
 import time
 from tools import print_metrics
 
@@ -126,6 +126,41 @@ class ReinforceActorCritic:
         print_metrics(range(episodes), scores_list, episode_times, action_times, actions_list, steps_per_game,
                       losses_per_episode)
 
+    def test(self, environment, episodes, max_steps):
+        win_games = 0
+        for e in trange(episodes, desc="Testing", unit="episode"):
+            state = environment.reset()
+            done = False
+            step_count = 0
+
+            if isinstance(environment, Farkle):
+                winner = environment.play_game(isBotGame=True, show=False, agentPlayer=self)
+                if winner == 0:
+                    win_games += 1
+
+            else:
+                while not environment.done and step_count < max_steps:
+                    available_actions = environment.available_actions()
+
+                    if hasattr(environment, "current_player") and environment.current_player == 1:
+                        action = self.choose_action(state, available_actions)
+                        step_count += 1
+                    else:
+                        action = random.choice(available_actions)
+
+                    next_state, reward, done = environment.step(action)
+                    state = next_state
+
+                    if environment.done and environment.winner == 1.0:
+                        win_games += 1
+                        break
+
+                if not done and step_count >= max_steps:
+                    print(f"Episode {e + 1}/{episodes} reached max steps ({max_steps})")
+        print(
+            f"Winrate:\n- {win_games} game wined\n- {episodes} game played\n- Accuracy : {(win_games / episodes) * 100:.2f}%"
+        )
+
     def update_baseline(self, state, delta):
         with tf.GradientTape() as tape:
             baseline_values = self.baseline(state, training=True)
@@ -152,7 +187,8 @@ class ReinforceActorCritic:
         return loss.numpy()
 
 
-_env = Farkle(printing=False)
-_model = ReinforceActorCritic(_env.state_size, _env.actions_size, 0.01, 0.001)
+# _env = Farkle(printing=False)
+# _model = ReinforceActorCritic(_env.state_size, _env.actions_size, 0.01, 0.001)
 
-_model.train(environment=_env, episodes=10, max_steps=300)
+# _model.train(environment=_env, episodes=2, max_steps=300)
+# _model.test(environment=_env, episodes=4, max_steps=300)
